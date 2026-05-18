b1761d26dff925fa867ee5c191550fbd53ec9bda
Gonzalo <gonzalo.millansar@gmail.com>
2026-05-18 17:56:38 -0300

    Funciones Gonza

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ def generar_pista(palabra):
     Ejemplo:
     "python" -> "p _ t _ o _"
     """
-    pass
+    
+    pista = ""
+
+    for i in range(len(palabra)):
+
+        if i % 2 == 0:
+            pista += palabra[i]
+        else:
+            pista += "_"
+
+        if i != len(palabra) - 1:
+            pista += " "
+
+    return pista
 
 
 # EJERCICIO 2
@@ -16,16 +29,35 @@ def verificar_palabra(palabra, intento):
     """
     Debe devolver True si el intento es correcto, False si no.
     """
-    pass
+    return palabra.lower() == intento.lower()
 
-
+# Ejercicio 3
 def calcular_puntaje(tiempo_restante, intentos_restantes):
-    pass
+    
+    return tiempo_restante * 10 + intentos_restantes * 20
 
 # Ejercicio Adicional
 def actualizar_pista(pista: str, palabra):
-    # Si me queda una letra por adivinar, no hago nada y devuelvo la misma pista que tenía
-    pass
+   def actualizar_pista(pista, palabra):
+
+    nueva_pista = ""
+
+    indice_letra = 0
+    reemplazado = False
+
+    for i in range(len(pista)):
+
+        if pista[i] == "_" and reemplazado == False:
+            nueva_pista += palabra[indice_letra]
+            reemplazado = True
+
+        else:
+            nueva_pista += pista[i]
+
+        if pista[i] != " ":
+            indice_letra += 1
+
+    return nueva_pista
 
 
 # ---------------- MAIN ----------------
