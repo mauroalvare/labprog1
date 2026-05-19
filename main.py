17e676aeba627a9a5ec19d2c75d0fb5d0b88b08a
Mauro Alvarez <mauroalvare@gmail.com>
2026-05-19 19:23:51 -0300

    Cambios Mauro

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from game import ejecutar_juego
 from palabras import obtener_palabra
 
+
 # EJERCICIO 1
 def generar_pista(palabra):
     """
@@ -8,7 +9,15 @@ def generar_pista(palabra):
     Ejemplo:
     "python" -> "p _ t _ o _"
     """
-    pass
+    pista = ""
+    for i in range(len(palabra)):
+        if i % 2 == 0:
+            pista += letra
+        else:
+            pista += "_"
+        if i < len(palabra) - 1:
+            pista += " "
+    return pista
 
 
 # EJERCICIO 2
@@ -16,25 +25,42 @@ def verificar_palabra(palabra, intento):
     """
     Debe devolver True si el intento es correcto, False si no.
     """
-    pass
+    return palabra.lower() == intento.lower()
 
 
 def calcular_puntaje(tiempo_restante, intentos_restantes):
-    pass
+    """
+    Calcula el puntaje basado en el tiempo restante e intentos restantes.
+    """
+    return tiempo_restante * 10 + intentos_restantes * 20
+
 
 # Ejercicio Adicional
-def actualizar_pista(pista: str, palabra):
-    # Si me queda una letra por adivinar, no hago nada y devuelvo la misma pista que tenía
-    pass
+def actualizar_pista(pista, palabra):
+
+    nueva_pista = ""
+
+    indice_letra = 0
+    reemplazado = False
+
+    for i in range(len(pista)):
+
+        if pista[i] == "_" and reemplazado == False:
+            nueva_pista += palabra[indice_letra]
+            reemplazado = True
+
+        else:
+            nueva_pista += pista[i]
+
+        if pista[i] != " ":
+            indice_letra += 1
+
+    return nueva_pista
 
 
 # ---------------- MAIN ----------------
 palabra = obtener_palabra()
 
 ejecutar_juego(
-    palabra,
-    generar_pista,
-    verificar_palabra,
-    actualizar_pista,
-    calcular_puntaje
+    palabra, generar_pista, verificar_palabra, actualizar_pista, calcular_puntaje
 )
